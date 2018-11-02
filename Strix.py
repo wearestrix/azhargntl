f7ae21cfa22218cfcb337d0796febbfb9f4778a9
azhargntl <44693925+azhargntl@users.noreply.github.com>
2018-11-02 14:09:14 +0700

    Create Strix.py
    ini belum jadi

---
+++ Strix.py
@@ -0,0 +1,100 @@
+## STRIX.py - STRIX v1.0
+# -*- coding: utf-8 -*-
+##
+import os
+import sys
+from time import sleep as timeout
+from core.strixcore import
+
+def main():
+        banner()
+		print "  [01] HACK MIni"
+		print "  [02] HACK Big"
+		print "  [03] HACK Aggresive"
+		print "  [04] Open Tools"
+		print "  [05] Exit"
+		stx= raw_input("stx > ")
+		
+		if stx == "1" or stx == "01"
+		    print "\n   [01] Fbbruteforce"
+			print "   [02] spammer-Email"
+			print "   [03]         "
+			print "   [00] kembali"
+			hackmini = raw_input("stx >")
+			
+			if hackmini == "01" hackmini == "1":
+			    Fbbruteforce()
+			elif hackmini == "02" or hackmini == "2":
+			    spammer_email
+			elif hackmini == "03" or hackmini == "3":
+			
+			elif hackmini == "00" or hackmini == "0":
+			   restart_program()
+			else:
+			     print "\nERROR: salah input"
+				 timeout(2)
+				 restart_program()
+				 
+		elif stx == "2" or == "02":
+		   	print "\n   [01] Webspolit"
+			print "   [02] SocialFIsh"
+			print "   [03] 
+			print "   [00] kembali"
+			hackbig = raw_input("stx >")
+			
+			if hackbig == "01" or hackbig == "1":
+			    Webspolit()
+			elif hackbig == "02" or hackbig == "2":
+			    SocialFIsh()
+			elif hackbig == "03" or hackbig == "3":
+			   
+			 elif hackbig == "00" or hackbig == "0":
+			     restart_program()
+			else:
+			    print "\nERROR: salah input"
+				timeout(2)
+				restart_program()
+				
+		elif stx == "3" or == "03":
+			print "\n    [01] 
+			print "    [02]
+			print "    [03]
+			print "    [00] kembali"
+			hackagrive = raw_input("stx >")
+		
+			if hackagrive == "01" or hackagrive =="1":
+		         
+			elif hackagrive == "02" or hackagrive =="2":
+
+			elif hackagrive == "02" or hackagrive == "2":
+			
+			elif hackagrive == "03" or hackagrive == "3":
+			
+			elif hackagrive == "00" or hackagrive == "0":
+			       restart_program()
+			else:
+			     print "\nERROR: salah input"
+				 timeout(2)
+				 restart_program()
+				 
+		elif stx == "4" or == "04":
+  		    print "\n    [01] Open Tools"
+			print "    [00] kembali"
+			opntools = raw_input("stx >")
+			
+			if opntools == "01" or opntools =="1":
+			   OpenTools()
+			else opntools == "00" or opntools == "1":
+			    restart_program()
+				
+		elif stx == "05":
+		    sys.exit()
+
+		else:
+             print "\nERROR: salah input"
+			 timeout(2)
+			 restart_program()
+			 
+	if _name_ == "_main_":
+          main()			
+		
